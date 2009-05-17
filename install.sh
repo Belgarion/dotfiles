dc9e2198bd0bff12ddf9b71c08b0fe2d6a9b7fc3
Sebastian <sebbe1991+git@gmail.com>
2009-05-17 13:45:03 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 dir=`pwd`
+mkdir $dir/old
 for i in *; do
-	[[ $i == "install.sh" ]] && continue
+	[[ $i == "install.sh" || $i == "README" ]] && continue
 	if [[ -f ~/.$i ]]; then
+		cp ~/.$i $dir/old/$i
+
 		echo "~/.$i already exists, overwrite? [y/n] "
 		read yn
 		if [[ $yn == "y" ]]; then
